46c9ec40043a79da25914dce02921ac0b4b44e0a
Varun G <gvsvarun@gmail.com>
2017-05-20 17:33:27 +0530

    feat: Added basic template for service

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+(function(global) {
+  /**
+   * Call to fetch data from Open Weather map
+   */
+  const generateWeatherData = ({ place, cntry = 'IN' }) => {
+    $.ajax(
+      `http://api.openweathermap.org/data/2.5/forecast?
+      q=${palce},${cntry}
+      &units=metric
+      &cnt=24
+      &appid=e83c0a3c38b673ca782b7dd77881c95b`
+    )
+      .done(function(resp) {
+        console.log(resp);
+      })
+      .fail(function() {
+        alert('error');
+      });
+  };
+  /**
+   * First function to be invoked by the application
+   */
+  const init = () => {
+    let place = 'Chennai';
+    let cntry = 'IN';
+    generateWeatherData({ place, cntry });
+  };
+  init();
+})(window);
